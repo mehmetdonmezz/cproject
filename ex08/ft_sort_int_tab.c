09e63d114c75cccd597b5cbd7ac3ba932d1893bc
Mehmet Donmez <mmehmetdonmez35342@gmail.com>
2025-02-22 16:04:52 +0300

    Mehmet Donmez C01

--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -32,7 +32,7 @@ void	ft_sort_int_tab(int *tab, int size)
 	}
 }
 
-int main()
+int main(void)
 {
     int arr[] = {5, 3, 8, 1, 2};
     int size = 5;
@@ -46,6 +46,4 @@ int main()
         i++;
     }
     printf("\n");
-    
-    return 0;
 }
