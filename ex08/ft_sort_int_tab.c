bad5ccfd9ee7e299798d9b74d5b30076cc4b4e8e
Mehmet Donmez <mmehmetdonmez35342@gmail.com>
2025-02-22 16:02:44 +0300

    a

--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -9,3 +9,43 @@
 /*   Updated: 2023/02/17 15:18:02 by ahtuncay         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	swap;
+
+	while(size >= 0)
+	{
+		i = 0;
+		while(i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				swap = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = swap;
+			}
+			i++;
+		}
+		size--;
+	}
+}
+
+int main()
+{
+    int arr[] = {5, 3, 8, 1, 2};
+    int size = 5;
+    
+    ft_sort_int_tab(arr, size);
+    
+    int i = 0;
+    while (i < size)
+    {
+        printf("%d ", arr[i]);
+        i++;
+    }
+    printf("\n");
+    
+    return 0;
+}
